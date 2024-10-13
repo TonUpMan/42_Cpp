9c99c9a34e053ce4a624f1cd2218c38fdf345bfa
tonupman <tonupman@work.myguest.virtualbox.org>
2024-10-13 11:47:52 +0200

    cpp8 start

---
+++ cpp8/ex00/easyFind.hpp
@@ -0,0 +1,19 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <exception>
+#include <algorithm>
+#include <iostream>
+#include <list>
+
+template<typename T>
+void    easyfind(T tab, int find){
+    
+    typename T::iterator it = std::find(tab.begin(), tab.end(), find);
+    if (it != tab.end())
+        std::cout << "value find: " << *it << std::endl;
+    else
+        throw(std::runtime_error("value not found"));
+}
+
+#endif
---
+++ cpp8/ex00/main.cpp
@@ -0,0 +1,34 @@
+#include "easyFind.hpp"
+
+int main(){
+
+    {
+        try{
+            std::list<int> tab;
+            tab.push_back(2);
+            tab.push_back(4);
+            tab.push_back(6);
+            tab.push_back(8);
+            tab.push_back(10);
+            ::easyfind(tab, 6);
+        }
+        catch(std::exception &e){
+            std::cout << e.what() << std::endl;
+        }
+    }
+    {
+        try{
+            std::list<int> tab;
+            tab.push_back(2);
+            tab.push_back(4);
+            tab.push_back(6);
+            tab.push_back(8);
+            tab.push_back(10);
+            ::easyfind(tab, 23);
+        }
+        catch(std::exception &e){
+            std::cout << e.what() << std::endl;
+        }
+    }
+
+}

04269ffe21075a2225ef0c0efb7504a5d6ef0d42
TonUpMan <qdevianne@gmail.com>
2024-10-23 09:25:21 +0200

    add main++

--- cpp6/ex01/main.cpp
+++ cpp6/ex01/main.cpp
@@ -6,9 +6,28 @@ int main(){
     uintptr_t   c;
 
     a.test = "i'm a test";
+    std::cout << "///////////////////////////////////// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << std::endl;
+    std::cout << "////////////////////////////// Serializer \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << std::endl;
+    std::cout << "///////////////////////////////////// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << std::endl;
+    std::cout << std::endl;
+    std::cout << "adress Data: " << &a << std::endl;
     std::cout << "data before serializer: " << a.test << std::endl;
+    std::cout << std::endl;
+    std::cout << "/////////////////////////// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << std::endl;
+    std::cout << "//////////////////// Serialization \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << std::endl;
+    std::cout << "/////////////////////////// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << std::endl;
+    std::cout << std::endl;
     c = Serializer::serialize(&a);
+    std::cout << "value uintptr: " << c << std::endl;
+    std::cout << "value hex : 0x" << std::hex << c << std::endl;
+    std::cout << std::endl;
+    std::cout << "/////////////////////////// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << std::endl;
+    std::cout << "//////////////////// Deserialization \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << std::endl;
+    std::cout << "/////////////////////////// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\" << std::endl;
+    std::cout << std::endl;
     Data *backup= Serializer::deserialize(c);
+    std::cout << "adress Data: " << &a << std::endl;
     std::cout << "data after serializer: " << backup->test <<std::endl;
+    std::cout << std::endl;
     return(0);
 }
--- cpp8/ex01/Span.cpp
+++ cpp8/ex01/Span.cpp
@@ -70,19 +70,3 @@ void    Span::fill_span(std::vector<int>::iterator min, std::vector<int>::iterat
         throw(std::runtime_error("span canno't receive all values"));
     _span.insert(_span.begin(), min, max);
 }
-
-
-/*
-Créez une classe Span pouvant stocker un maxium de N entiers. N est une variable de
-type entier non-signé et sera le seul paramètre passé au constructeur.
-Cette classe aura une fonction membre appelée addNumber() afin d’ajouter un seul
-nombre à la Span. On l’utilisera pour remplir cette dernière. Toute tentative d’ajouter
-un nouvel élément s’il y en a déjà N autres stockés jettera une exception.
-Ensuite, implémentez deux fonctions membres : shortestSpan() et longestSpan()
-Elles devront respectivement trouver la plus petite distance et la plus grande distance
-entre les nombres stockés, puis la retourner. S’il n’y a aucun nombre stocké, ou juste un,
-aucune distance ne peut être trouvée. Par conséquent, jetez une exception.
-Bien sûr, implémentez vos propres tests qui devront être bien plus complets que celui
-donné ci-dessous. Testez votre Span avec au moins 10 000 nombres. Vous pouvez tester
-avec plus de nombres, c’est encore mieux.
-*/

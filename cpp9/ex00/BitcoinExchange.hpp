19718c0a532f1d14b014984e042f2b0a34eae96f
TonUpMan <qdevianne@gmail.com>
2024-10-28 14:57:31 +0100

    reset

--- cpp9/ex00/BitcoinExchange.hpp
+++ cpp9/ex00/BitcoinExchange.hpp
@@ -1,21 +1,5 @@
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 
-#include <iostream>
-#include <fstream>
-#include <map>
-
-class BitcoinExchange{
-
-    public:
-        BitcoinExchange();
-        BitcoinExchange(BitcoinExchange const &cpy);
-        ~BitcoinExchange();
-        BitcoinExchange const &operator=(BitcoinExchange const &rhs);
-
-        void    set_ref(std::string data);
-    private:
-        std::map<std::string, int>    ref; 
-};
 
 #endif
--- cpp9/ex00/main.cpp
+++ cpp9/ex00/main.cpp
@@ -1,18 +1,5 @@
 #include "BitcoinExchange.hpp"
 
 int main(int ac, char **av){
-    (void)av;
-    if(ac == 1){
-        try{
-            BitcoinExchange();
-        }
-        catch(std::exception &e){
-            return(0);
-        }
-    }
-    else{
-        std::cout << "error: number of argument" << std::endl;
-        return(0);
-    }
-    return(1);
+
 }

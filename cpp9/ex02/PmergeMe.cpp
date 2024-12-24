cb97e39f33f29101a20b7949699bf07b87c53586
TonUpMan <qdevianne@gmail.com>
2024-12-24 13:02:34 +0100

    binary search ok

--- cpp9/ex02/PmergeMe.cpp
+++ cpp9/ex02/PmergeMe.cpp
@@ -1,5 +1,47 @@
 #include "PmergeMe.hpp"
 # include <sys/time.h>
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+template <typename T>
+void	print(T cont){
+	if(cont.size() <= 15){
+		typename T::iterator it;
+		for(it = cont.begin(); it != cont.end(); it++){
+			std::cout << *it << " ";
+		}
+		std::cout << std::endl;
+	}
+	else{
+		int limit = 5;
+		typename T::iterator it;
+		for(it = cont.begin(); limit; it++){
+			std::cout << *it << " ";
+			limit--;
+		}
+		std::cout << "[...]";
+		limit = 5;
+		typename T::reverse_iterator rit;
+		for(rit = cont.rbegin(); limit > 0 && rit != cont.rend(); --rit){
+			std::cout << *rit << " ";
+			limit--;
+		}
+		std::cout<< std::endl;
+	}
+}
+
+template <typename T>
+T	sequenceGenerate(int n, int start){
+	T sequence;
+	sequence.push_back(0);
+	sequence.push_back(start);
+	for(int i = 2; i < n; i++){
+		int tmp = sequence[i - 1] + (2 * sequence[i - 2]);
+		sequence.push_back(tmp);
+	}
+	return(sequence);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 PmergeMe::PmergeMe(){}
 
@@ -17,8 +59,6 @@ PmergeMe const &PmergeMe::operator=(PmergeMe const &rhs){
 	}
 	return(*this);
 }
-
-void	PmergeMe::setStart(){_start = timer();}
 
 void	PmergeMe::setVec(std::string arg){
 
@@ -42,33 +82,19 @@ void	PmergeMe::setLst(std::string arg){
 	}
 }
 
+void	PmergeMe::setStart(){_start = timer();}
 void	PmergeMe::setTimeVec(){_timeVec = timer() - _start;}
 void	PmergeMe::setTimeLst(){_timeLst = timer() - _start;}
 
 std::vector<int> const	&PmergeMe::getVec() const{return(_vec);}
 std::list<int> const	&PmergeMe::getLst() const{return(_lst);}
 
-long int	timer(void){
-
-	struct timeval	tv;
-	long int		tmp;
-
-	gettimeofday(&tv, NULL);
-	tmp = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-	std::cout << tmp << std::endl;
-	return (tmp);
-}
-
-void				PmergeMe::vecSort(std::string arg){
+void	PmergeMe::vecSort(std::string arg){
 	setStart();
 	setVec(arg);
 	std::vector<int> result;
 	result = recursiveSortVec(_vec);
-	std::vector<int>::iterator it;
-	for(it = result.begin(); it != result.end(); it++){
-		std::cout << *it << " ";
-	}
-	std::cout << std::endl;
+	print(result);
 	setTimeVec();
 }
 
@@ -87,9 +113,8 @@ std::vector<int>	PmergeMe::recursiveSortVec(std::vector<int> arg){
 	}
 	if (arg.size() % 2 != 0){
     	int last = arg.back();
-    	if (maxValue.empty() || last > *std::max_element(maxValue.begin(), maxValue.end())) {
+    	if (maxValue.empty() || last > *std::max_element(maxValue.begin(), maxValue.end()))
         	maxValue.push_back(last);
-    	}
 		else
        		minValue.push_back(last);
 	}
@@ -98,39 +123,39 @@ std::vector<int>	PmergeMe::recursiveSortVec(std::vector<int> arg){
 	return(insertMerge(minValue, maxValue));
 }
 
-std::vector<int>	sequenceGenerate(int n, int start){
-	std::vector<int> sequence;
-	sequence.push_back(0);
-	sequence.push_back(start);
-	for(int i = 2; i < n; i++){
-		int tmp = sequence[i - 1] + (2 * sequence[i - 2]);
-		sequence.push_back(tmp);
-	}
-	return(sequence);
-}
-
 std::vector<int>	PmergeMe::insertMerge(std::vector<int> minValue, std::vector<int> maxValue){
-	//std::vector<int>	jacob = sequenceGenerate(minValue.size(), 1);
-	//std::vector<int>	ford = sequenceGenerate(minValue.size(), 2);
-	if(minValue.size() == 1 && maxValue.size() == 1){
-		maxValue.insert(minValue.begin(), minValue[0]);
-		return(maxValue);
-	}
+	// if(minValue.size() == 1 && maxValue.size() == 1){
+	// 	maxValue.insert(maxValue.begin(), minValue[0]);
+	// 	return(maxValue);
+	// }
 	for(size_t i = 0; i < minValue.size(); i++){
-		std::vector<int>::iterator it = maxValue.end();
-		while (it != maxValue.begin()){
-		    --it;
-		    if (minValue[i] >= *it){
-		        ++it;
-		        break;
-		    }
+		int start = 0;
+		int end = maxValue.size();
+		while(start <= end){
+			int middle = start + ((end - start) / 2);
+			if(minValue[i] == maxValue[middle]){
+				start = middle;
+				break ;
+			}
+			else if(minValue[i] < maxValue[middle])
+				end = middle - 1;
+			else
+				start = middle + 1;
 		}
+		std::vector<int>::iterator it;
+		for(it = maxValue.begin(); *it != maxValue[start]; it++)
+			;
 		maxValue.insert(it, minValue[i]);
 	}
 	return(maxValue);
 }
 
+long int	timer(void){
 
-//void				lstSort(std::string arg){}
-//std::list				*initLstPair(){}
-//void	PmergeMe::recursiveSortLst(){}
+	struct timeval	tv;
+	long int		tmp;
+
+	gettimeofday(&tv, NULL);
+	tmp = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	return (tmp);
+}
